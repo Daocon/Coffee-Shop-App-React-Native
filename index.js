fae699d48cb81017b957156216ff3f4779d6dffd
Daocon <hadao1204@gmail.com>
2024-02-20 12:43:56 +0700

    upload

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import SettingScreen from './src/screens/SettingScreen';
+import PersonScreen from './src/screens/PersonScreen';
 
 AppRegistry.registerComponent(appName, () => App);
